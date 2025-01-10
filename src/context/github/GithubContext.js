c420af914e27a09c6c367b798e96f6c7ddf60dea
ch9729 <karlll@naver.com>
2025-01-10 12:34:27 +0900

    변경 내용 수정

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -2,9 +2,6 @@ import { createContext, useState } from "react";
 
 const GithubContext = createContext();
 
-const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
-
 // 프로바이더가 전역으로 컨텍스트를 적용함
 export const GithubProvider = ({ children }) => {
   const [users, setUsers] = useState([]);
@@ -15,11 +12,14 @@ export const GithubProvider = ({ children }) => {
   const searchUsers = (text) => {
     setLoading(true);
     const params = new URLSearchParams({ q: text });
-    const response = fetch(`${GITHUB_URL}/search/users?${params}`, {
-      headers: {
-        Authorization: `Bearer ${GITHUB_TOKEN}`,
-      },
-    })
+    const response = fetch(
+      `${process.env.REACT_APP_GITHUB_URL}/search/users?${params}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.REACT_APP_GITHUB_TOKEN}`,
+        },
+      }
+    )
       .then((res) => res.json())
       .then((data) => {
         setUsers(data.items);
@@ -32,11 +32,14 @@ export const GithubProvider = ({ children }) => {
   const getUser = (login) => {
     setLoading(true);
 
-    const response = fetch(`${GITHUB_URL}/users/${login}`, {
-      headers: {
-        Authorization: `Bearer ${GITHUB_TOKEN}`,
-      },
-    })
+    const response = fetch(
+      `${process.env.REACT_APP_GITHUB_URL}/users/${login}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.REACT_APP_GITHUB_TOKEN}`,
+        },
+      }
+    )
       .then((res) => res.json())
       .then((data) => {
         setUser(data);
@@ -58,11 +61,14 @@ export const GithubProvider = ({ children }) => {
       sort: "created",
       per_page: 10,
     });
-    const response = fetch(`${GITHUB_URL}/users/${login}/repos?${params}`, {
-      headers: {
-        Authorization: `Bearer ${GITHUB_TOKEN}`,
-      },
-    })
+    const response = fetch(
+      `${process.env.REACT_APP_GITHUB_URL}/users/${login}/repos?${params}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.REACT_APP_GITHUB_TOKEN}`,
+        },
+      }
+    )
       .then((res) => res.json())
       .then((data) => {
         setRepos(data);
